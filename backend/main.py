15052fe901c9ab1a059303b79f997584ab7a6433
Ali Coskun <acoskunbiz@gmail.com>
2022-09-24 22:58:27 +0100

    Complete DB tables

--- backend/main.py
+++ backend/main.py
@@ -7,29 +7,49 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+#Association table -> (M-M) Project & Donation
+donation_project = db.Table('donation_project', db.Column('trId', db.Integer, db.ForeignKey('donation.trId')), db.Column('prId', db.Integer, db.ForeignKey('project.projectId')))
+
 class Donor(db.Model):
     __tablename__ = 'donor'
-    donorId = db.Column('id', db.Integer, primary_key=True)
-    fname = db.Column('first_name', db.String(100))
-    lname = db.Column('last_name', db.String(100))
-    email = db.Column('email', db.String(100))
+    #Donor ID -> Primary Key
+    donorId = db.Column('donor_id', db.Integer, primary_key=True)
+    fname = db.Column('first_name', db.String(255), nullable=False)
+    lname = db.Column('last_name', db.String(255), nullable=False)
+    email = db.Column('email', db.String(255), nullable=False)
+    #backref relationship -> Donation
+    donations = db.relationship('Donor', backref='donor')
 
 class Donation(db.Model):
     __tablename__ = 'donation'
     #Transaction ID -> primary key
     trId = db.Column('transaction_id', db.Integer, primary_key=True)
-    #Donor ID -> foreign key
-    donor_id = db.Column(db.Integer, db.ForeignKey('donation.donorId'))
-    amount = db.Column('', db.Numeric)
-    currency = db.Column('currency', db.String(3))
-
-
+    #Donor ID -> foreign key (1-M)
+    donor_id = db.Column(db.Integer, db.ForeignKey('donor.donorId'))
+    amount = db.Column('amount', db.Integer, nullable=False)
+    currency = db.Column('currency', db.String(3), nullable=False)
+    #backref relationship -> Project
+    projects = db.relationship('Project', secondary=donation_project, backref='project')
+    
 
 class Project(db.Model):
-    __tablename__ = 'Project'
-    projectId = db.Column('id',db.Integer, primary_key=True)
-    projectName = db.Column('project_name', db.String(100))
-    projectLocation = db.Column('project_location', db.String(100))
+    __tablename__ = 'project'
+    #Project ID -> primary key
+    projectId = db.Column('project_id', db.Integer, primary_key=True)
+    projectName = db.Column('project_name', db.String(255))
+    projectLocation = db.Column('project_location', db.String(255))
+    #backref relationship -> Task
+    tasks = db.relationship('Task', backref='task')
+
+class Task(db.Model):
+    __tablename__ = 'task'
+    #Task ID -> primary key
+    taskId = db.Column('task_id', db.Integer, primary_key=True )
+    taskName = db.Column('task_name', db.String(255), nullable=False)
+    cost = db.Column('cost', db.Integer, nullable=False)
+    totalPaid = db.Column('paid', db.Integer)
+    #Project ID -> foreign key (1-M)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.projectId'))
 
 
 dbExample = [
